32dd2df024b5f272ecacacdfef83b6ba20df15c7
vinnisntos <vinnicius.sa@aluno.unip.br>
2025-10-29 19:21:47 -0300

    ss

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve a pasta atual como pública
+// Serve a pasta atual
 app.use(express.static(__dirname));
 
-// Inicia o servidor
+// Rota específica para o QR Code
+app.get("/qrcode.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "qrcode.png"));
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
