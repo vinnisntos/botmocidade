b564dbc01734963af4269448f54bb83bec8150f7
vinnisntos <vinnicius.sa@aluno.unip.br>
2025-10-29 17:45:39 -0300

    ref

--- commands/oi.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'oi',
-    execute(msg) {
-        msg.reply("Oi! Tudo bem?");
-    }
-};
--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "!ping",
-    execute(msg) {
-        msg.reply("🏓 Pong!");
-    }
-};
-
